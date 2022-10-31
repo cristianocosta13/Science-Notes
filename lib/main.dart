08f314b4586c1b3d803dca541d001809035acf7d
cristianocosta13 <103438234+cristianocosta13@users.noreply.github.com>
2022-10-31 10:57:55 -0300

    Add files via upload
    inserção da lib atualizada.

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:sciencenotes/pages/enter_page.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: EnterPage(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
