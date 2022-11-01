467cca6ff4a842b49401bd9eae28fb3058e335aa
Júlia Ferreira <103438145+fjuliaaf@users.noreply.github.com>
2022-11-01 09:09:46 -0300

    Add files via upload
    atualização da lib atualizada com a inserção da funcionalidade atualizada

---
+++ lib/widgets/favorites_card.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:sciencenotes/assets/colors/custom_colors.dart';
+import 'package:sciencenotes/domain/favorites.dart';
+
+class FavoriteCard extends StatefulWidget {
+  final Favorites fav;
+  const FavoriteCard({super.key, required this.fav});
+
+  @override
+  State<FavoriteCard> createState() => _FavoriteCardState();
+}
+
+class _FavoriteCardState extends State<FavoriteCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: CustomColors.gradienColor,
+      child: Column(children: [
+        Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            widget.fav.nameDiscipline,
+            style: const TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+              fontFamily: 'Staatliches',
+            ),
+          ),
+          const Icon(
+            Icons.favorite,
+            color: Colors.red,
+          ),
+        ],
+      ),
+      const Divider(),
+      const SizedBox(height: 16,),
+      ],)
+    );
+  }
+}
