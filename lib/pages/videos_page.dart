13381e543e7fd2f2070e19fad27dd8c12022f618
MariaLorrana913 <103418874+LorranaA1@users.noreply.github.com>
2022-11-28 09:04:35 -0300

    Update videos_page.dart

--- lib/pages/videos_page.dart
+++ lib/pages/videos_page.dart
@@ -1,7 +1,11 @@
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:sciencenotes/assets/colors/custom_colors.dart';
+import 'package:sciencenotes/data/video_dao.dart';
+import 'package:sciencenotes/domain/videos.dart';
 import 'package:sciencenotes/widgets/video_card.dart';
-import '../domain/content.dart';
+import 'package:sciencenotes/pages/addVideo.dart';
+import 'package:sciencenotes/domain/content.dart';
 
 class VideosPage extends StatefulWidget {
   final Content conteudo;
@@ -19,24 +23,51 @@ class _VideosPage extends State<VideosPage> {
   //List<VideoCardWidget> lista = DBcontent.listaVideos;
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        VideoCardWidget(
-          content: widget.conteudo,
-          index: 0,
-        ),
-        const SizedBox(height: 4),
-        VideoCardWidget(
-          content: widget.conteudo,
-          index: 1,
-        ),
-        const SizedBox(height: 4),
-        VideoCardWidget(
-          content: widget.conteudo,
-          index: 2,
-        ),
-        const SizedBox(height: 4),
-      ],
+    return Scaffold(
+      body: ListView(
+        children: [
+          buildListView(),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context){
+              return const AddVideo();
+            },
+          ),
+        );
+      },
+        child: Icon(Icons.add),
+        backgroundColor: CustomColors.appeButtonColor,
+      ),
+      backgroundColor: CustomColors.gradienColor,
+    );
+  }
+
+  buildListView() {
+    var list = VideosDao().selectDiscipline(widget.conteudo);
+    return FutureBuilder<List<Videos>>(
+      future: list,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          // ?? -> Verificar ser o conteudo de snapshot.data é nulo
+          List<Videos> list = snapshot.data ?? [];
+
+          return ListView.builder(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: list.length,
+            itemBuilder: (BuildContext context, int index) {
+              return VideoCardWidget(video: list[0],);
+            },
+          );
+        }
+
+        return const Center(child: CircularProgressIndicator());
+      },
     );
   }
 }
