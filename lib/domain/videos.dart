1d9c64ff8bb7e62606ff27c9eee60e48447a98fc
MariaLorrana913 <103418874+LorranaA1@users.noreply.github.com>
2022-11-28 08:59:46 -0300

    Update videos.dart

--- lib/domain/videos.dart
+++ lib/domain/videos.dart
@@ -1,49 +1,39 @@
-class Video {
+import 'package:flutter/material.dart';
+
+class Videos {
   late int id;
   late String urlImage;
-  late String title;
-  late String duration;
+  late String tituloVideo;
+  late String tempoDuracao;
   late String urlVideo;
   late int idContent;
 
-  Video(
-      {required this.id,
-        required this.urlImage,
-        required this.title,
-        required this.duration,
-        required this.urlVideo,
-        required this.idContent});
+  Videos({
+    required this.id,
+    required this.urlImage,
+    required this.tituloVideo,
+    required this.tempoDuracao,
+    required this.urlVideo,
+    required this.idContent,
+  });
 
-  Video.fromJson(Map<String, dynamic> json) {
+  Videos.fromJson(Map<String, dynamic> json) {
     id = json['id'];
-    urlImage = json['url_image'];
-    title = json['title'];
-    duration = json['duration'];
-    urlVideo = json['url_video'];
-    idContent = json['id_content'];
+    urlImage = json['urlImage'];
+    tituloVideo = json['tituloVideo'];
+    tempoDuracao = json['tempoDuracao'];
+    urlVideo = json['urlVideo'];
+    idContent = json['idContent'];
   }
 
-  Map<String, dynamic> toJson() {
+  Map<String, Object?> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['id'] = this.id;
-    data['url_image'] = this.urlImage;
-    data['title'] = this.title;
-    data['duration'] = this.duration;
-    data['url_video'] = this.urlVideo;
-    data['id_content'] = this.idContent;
+    data['urlImage'] = urlImage;
+    data['tituloVideo'] = tituloVideo;
+    data['tempoDuracao'] = tempoDuracao;
+    data['urlVideo'] = urlVideo;
+    data['idContent'] = this.idContent;
     return data;
   }
 }
-
-// class Videos {
-//   String urlImage;
-//   String tituloVideo;
-//   String tempoDuracao;
-//   String urlVideo;
-//
-//   Videos(
-//       {required this.urlImage,
-//       required this.tituloVideo,
-//       required this.tempoDuracao,
-//       required this.urlVideo});
-// }
