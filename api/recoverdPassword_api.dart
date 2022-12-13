52c9e04174a1e7b35710943aeb08e02cbac24081
MariaLorrana913 <103418874+LorranaA1@users.noreply.github.com>
2022-12-13 09:13:30 -0300

    Add files via upload

---
+++ api/recoverdPassword_api.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+import 'package:sciencenotes/pages/enter_page.dart';
+
+class RecoverdPasswordApi {
+  final String _baseUrl = "https://science-notes.joscristiano.repl.co/";
+
+  Future<bool> findByUser(String username, String password) async {
+    Uri url = Uri.http(_baseUrl, "/recoverPassword/$email/$newPassword");
+    Response response = await http.get(url);
+
+    // TESTE
+    print(response.statusCode);
+    print(response.body);
+
+    var json = jsonDecode(response.body);
+    if (json['logado'] == true) {
+      return true;
+    }
+    return false;
+  }
+}
